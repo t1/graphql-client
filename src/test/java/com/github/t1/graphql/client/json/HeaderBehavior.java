a2de11a35dc3f31438a7c3a9332e3b7e56e7dcba
Rüdiger zu Dohna <snackbox@sinntr.eu>
2020-04-09 17:32:28 +0200

    add GraphQlClientAuthorizationHeader

--- src/test/java/com/github/t1/graphql/client/json/HeaderBehavior.java
+++ src/test/java/com/github/t1/graphql/client/json/HeaderBehavior.java
@@ -1,5 +1,7 @@
 package com.github.t1.graphql.client.json;
 
+import com.github.t1.graphql.client.api.GraphQlClientApi;
+import com.github.t1.graphql.client.api.GraphQlClientAuthorizationHeader;
 import com.github.t1.graphql.client.api.GraphQlClientHeader;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +15,7 @@ class HeaderBehavior {
     private final GraphQlClientFixture fixture = new GraphQlClientFixture();
 
     interface StringApi {
+        @SuppressWarnings("UnusedReturnValue")
         String greeting();
     }
 
@@ -25,12 +28,61 @@ class HeaderBehavior {
             .header(new GraphQlClientHeader("H3", clock::instant))
             .build(StringApi.class);
 
-        String greeting = api.greeting();
+        api.greeting();
 
-        then(fixture.query()).isEqualTo("greeting");
         then(fixture.sentHeader("H1")).isEqualTo("V1");
         then(fixture.sentHeader("H2")).isEqualTo("V2");
         then(fixture.sentHeader("H3")).isEqualTo(clock.instant());
-        then(greeting).isEqualTo("dummy-greeting");
+    }
+
+
+    @Test void shouldLoadNonAnnotatedKeyConfigAuthHeader() {
+        shouldConfigureAuthHeader("", StringApi.class,
+            new GraphQlClientAuthorizationHeader());
+    }
+
+
+    @Test void shouldLoadNonAnnotatedKeyConfigApiAuthHeader() {
+        shouldConfigureAuthHeader(StringApi.class.getName() + "/mp-graphql/",
+            StringApi.class,
+            new GraphQlClientAuthorizationHeader(StringApi.class));
+    }
+
+
+    @GraphQlClientApi
+    interface AnnotatedStringApi extends StringApi {}
+
+    @Test void shouldLoadAnnotatedKeyConfigApiAuthHeader() {
+        shouldConfigureAuthHeader(AnnotatedStringApi.class.getName() + "/mp-graphql/",
+            AnnotatedStringApi.class,
+            new GraphQlClientAuthorizationHeader(AnnotatedStringApi.class));
+    }
+
+
+    @GraphQlClientApi(configKey = "pre")
+    interface ConfiguredKeyStringApi extends StringApi {}
+
+    @Test void shouldLoadConfiguredKeyConfigApiAuthHeader() {
+        shouldConfigureAuthHeader("pre/mp-graphql/", ConfiguredKeyStringApi.class,
+            new GraphQlClientAuthorizationHeader(ConfiguredKeyStringApi.class));
+    }
+
+
+    private void shouldConfigureAuthHeader(String expectedConfigKey, Class<? extends StringApi> apiClass, GraphQlClientAuthorizationHeader header) {
+        System.setProperty(expectedConfigKey + "username", "foo");
+        System.setProperty(expectedConfigKey + "password", "bar");
+        try {
+            fixture.returnsData("'greeting':'dummy-greeting'");
+            StringApi api = fixture.builder()
+                .header(header)
+                .build(apiClass);
+
+            api.greeting();
+
+            then(fixture.sentHeader("Authorization")).isEqualTo("Basic Zm9vOmJhcg==");
+        } finally {
+            System.clearProperty(expectedConfigKey + "username");
+            System.clearProperty(expectedConfigKey + "password");
+        }
     }
 }
